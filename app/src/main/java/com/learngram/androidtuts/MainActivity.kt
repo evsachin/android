f057715d546672b99f1b26e6e7db6d97d5904a17
wtsachin <sachin.kolhe.evisiontech@gmail.com>
2025-01-28 21:51:24 +0530

    Edit text Toast Intent navigating to next activity

--- app/src/main/java/com/learngram/androidtuts/MainActivity.kt
+++ app/src/main/java/com/learngram/androidtuts/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.learngram.androidtuts
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,10 +17,24 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Initialization of views
         val btnShowToast : Button = findViewById(R.id.btnShowToast)
+        val etUserMessage: EditText = findViewById(R.id.etUserMessage)
+
+
+
+       val btnSendMessageToNextActivity =  findViewById<Button>(R.id.btnSendMessageToNextActivity)
         btnShowToast.setOnClickListener {
             Log.i("Main Activity","Show btn was clicked")
             Toast.makeText(this,"Button was clicked !!",Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMessageToNextActivity.setOnClickListener {
+            val message = etUserMessage.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
